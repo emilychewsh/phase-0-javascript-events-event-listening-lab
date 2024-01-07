b6025dec2714a81331e71f7f5d702c2eced688d4
Emily Chew <emilychewsh@gmail.com>
2024-01-07 14:10:54 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('button')
+
 function addingEventListener() {
+    input.addEventListener('click', alertMe)
+}
+
+function alertMe(){
+    alert('I was clicked and called!')
 }
